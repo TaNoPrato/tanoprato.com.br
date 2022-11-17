f19bf9136a58dfdbdef4076419147a7af2e2af35
TaNoPrato <118411162+TaNoPrato@users.noreply.github.com>
2022-11-16 23:26:43 -0300

    Add files via upload

---
+++ js/sistema.js
@@ -0,0 +1,90 @@
+
+var json = [
+    {
+        "id": "1",
+        "nome": "Panqueca de Banana",
+        "ingrediente": [
+            "Banana", "Ovo", "Açúcar", "Farinha de Trigo", "Leite"
+        ]
+    },
+    {
+        "id": "2",
+        "nome": "Bolo de Milho",
+        "ingrediente": [
+            "Milho", "Leite", "Açúcar", "Óleo", "Ovo", "Fermento em Pó"
+        ]
+    },
+    {
+        "id": "3",
+        "nome": "Salada de Ovo",
+        "ingrediente": [
+            "Cebola", "Batata", "Maionese", "Óleo", "Ovo"
+        ]
+    },
+    {
+        "id": "4",
+        "nome": "Bolo de Fubá",
+        "ingrediente": [
+            "Ovo", "Açúcar", "Fubá", "Farinha de Trigo", "Óleo", "Leite", "Fermento em Pó"
+        ]
+    },
+    {
+        "id": "5",
+        "nome": "Hambúrguer de Frango",
+        "ingrediente": [
+            "Frango", "Cebola", "Farinha de Rosca", "Ovo", "Maionese", "Óleo"
+        ]
+    },
+    {
+        "id": "6",
+        "nome": "Banana á Milanesa",
+        "ingrediente": [
+            "Banana", "Farinha de Trigo", "Farinha de Rosca", "Ovo", "Açúcar", "Óleo"
+        ]
+    },
+    {
+        "id": "7",
+        "nome": "Frango á Milanesa",
+        "ingrediente": [
+            "Frango", "Farinha de Rosca", "Farinha de Trigo", "Ovo "
+        ]
+    },
+    {
+        "id": "8",
+        "nome": "Sanduíche Natural",
+        "ingrediente": [
+            "Frango", "Maionese", "Milho, "
+        ]
+    },
+    {
+        "id": "9",
+        "nome": "Creme de Milho",
+        "ingrediente": [
+            "Milho", "Leite", "Óleo", "Cebola"
+        ]
+    },
+    {
+        "id": "10",
+        "nome": "Massa de Torta",
+        "ingrediente": [
+            "Óleo", "Ovo", "Farinha de Trigo", "Leite", "Fermento em Pó"
+        ]
+    },
+    {
+        "id": "11",
+        "nome": "Suspiro",
+        "ingrediente": [
+            "Açúcar", "Ovo"
+        ]
+    },
+    {
+        "id": "12",
+        "nome": "Batata Frita",
+        "ingrediente": [
+            "Batata", "Óleo"
+        ]
+    }
+
+
+
+]
